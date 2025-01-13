94690c4af32df3258832eb9ca0d2c9587bab7e5b
PieStw <piero.stalteri@icloud.com>
2025-01-13 15:51:44 +0100

    creazione homePage

--- src/contex/MovieContext.jsx
+++ src/contex/MovieContext.jsx
@@ -1,9 +1,32 @@
-import { createContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 
 const MovieContext = createContext();
 
 export const useMovieContext = () => useContext(MovieContext);
 
 export const MovieProvider = ({ children }) => {
-  return <MovieContext.Provider value={{}}>{children}</MovieContext.Provider>;
+  const [movie, setMovie] = useState([]);
+  const [movieList, setMovieList] = useState([]);
+
+  const indexMovies = () => {
+    fetch(`http://localhost:3000/movies`)
+      .then((res) => res.json())
+      .then((res) => setMovieList(res));
+  };
+
+  const showMovie = (id) => {
+    fetch(`http://localhost:3000/movies/${id}`)
+      .then((res) => res.json())
+      .then((res) => setMovie(res));
+  };
+
+  useEffect(() => {
+    indexMovies();
+  }, []);
+
+  return (
+    <MovieContext.Provider value={{ movie, movieList, showMovie }}>
+      {children}
+    </MovieContext.Provider>
+  );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { useMovieContext } from "../contex/MovieContext";
+import styles from "../assets/css/HomePage.module.css";
 
 export default function HomePage() {
+  const { movie, movieList, showMovie } = useMovieContext();
   return (
     <>
-      <h1>CIAO</h1>
+      <div className={styles.cardContainer}>
+        {movieList.map((movie) => (
+          <div key={movie.id} className={styles.card}>
+            <img src={movie.image} class={styles.image} />
+            <div className="card-body">
+              <h5 className="card-title">Title: {movie.title}</h5>
+              <p className="card-text">Abstract: {movie.abstract}</p>
+              <p className="card-text">Release Year: {movie.release_year}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 }

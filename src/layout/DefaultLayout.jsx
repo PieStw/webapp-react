17737f631bac8f16ab25ea6419e70ba4e860dcfd
PieStw <piero.stalteri@icloud.com>
2025-01-13 15:05:11 +0100

    creazione layout

---
+++ src/layout/DefaultLayout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/navbar/Navbar";
+
+export default function DefaultLayout() {
+  return (
+    <>
+      <div>
+        <header>
+          <NavBar></NavBar>
+        </header>
+
+        <main>
+          <Outlet></Outlet>
+        </main>
+      </div>
+    </>
+  );
+}

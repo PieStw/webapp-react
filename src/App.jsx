a86e15f0d4e6f8ff38eb56a7789c174ffc38aa0c
PieStw <piero.stalteri@icloud.com>
2025-01-13 15:12:55 +0100

    aggiunta route e homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DefaultLayout from "./layout/DefaultLayout";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DefaultLayout}>
+            <Route path="/" element={<HomePage />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;

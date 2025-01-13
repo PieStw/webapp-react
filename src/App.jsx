3e6bf6a17599e8c79165171829a5988faa3cfe5d
PieStw <piero.stalteri@icloud.com>
2025-01-13 16:16:46 +0100

    aggiunto path

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DefaultLayout from "./layout/DefaultLayout";
 import { MovieProvider } from "./contex/MovieContext";
 import HomePage from "./pages/HomePage";
+import FilmPage from "./pages/FilmPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route Component={DefaultLayout}>
               <Route path="/" element={<HomePage />}></Route>
+              <Route path="/movie/:id" element={<FilmPage />}></Route>
             </Route>
           </Routes>
         </BrowserRouter>
